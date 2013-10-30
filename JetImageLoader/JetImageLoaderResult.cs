ebf29df06bb3538175dd7d93f27b05d1fcc1e977
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2013-10-30 16:31:25 +0400

    [feature]  JetImageLoaderResult prototype

--- JetImageLoader/JetImageLoaderResult.cs
+++ JetImageLoader/JetImageLoaderResult.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
+using System;
+using System.IO;
 namespace JetImageLoader
 {
-    class JetImageLoaderResult
+    public class JetImageLoaderResult
     {
+        /// <summary>
+        /// Possible sources of ImageStream
+        /// </summary>
+        public enum Source
+        {
+            Network,
+            StorageCache,
+            MemoryCache,
+            Assets
+        }
+
+        public Stream ImageStream { get; set; }
+
+        public Source ImageStreamSource { get; set; }
+
+        public Uri ImageUri { get; set; }
     }
 }
